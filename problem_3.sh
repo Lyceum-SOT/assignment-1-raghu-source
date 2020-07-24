5d1b12df87ec9033fe124b0c1595eb9d24fbf2a7
raghu-source <cadetraghuprasad205250@gmail.com>
2020-07-24 15:13:35 +0530

    All 3 problems added

---
+++ problem_3.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+gcc -o problem_3.txt pi.c #problem_3.txt has the output of pi.c in executable form( executable using : ./problem_3.txt")
+
+./problem_3.txt >> prob3.txt # the text form of the output of pi.c is in prob3.txt
+
